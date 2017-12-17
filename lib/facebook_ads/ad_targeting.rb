bfe3dfe86dc932b02a050a4bd0f7ca384aabce4e
Geoff Wright <geoff@wright>
2017-12-17 16:43:04 +0700

    custom_audiences

--- lib/facebook_ads/ad_targeting.rb
+++ lib/facebook_ads/ad_targeting.rb
@@ -14,7 +14,7 @@ module FacebookAds
     NOT_INSTALLED      = 'not_installed'.freeze
     APP_INSTALL_STATES = [INSTALLED, NOT_INSTALLED].freeze
 
-    attr_accessor :genders, :age_min, :age_max, :countries, :user_os, :user_device, :app_install_state, :custom_locations, :income, :publisher_platforms, :facebook_positions
+    attr_accessor :genders, :age_min, :age_max, :countries, :user_os, :user_device, :app_install_state, :custom_locations, :income, :publisher_platforms, :facebook_positions, :custom_audiences
 
     def initialize
       # self.genders = [WOMEN] # If nil, defaults to all genders.
@@ -66,6 +66,7 @@ module FacebookAds
         income: income,
         publisher_platforms: publisher_platforms,
         facebook_positions: facebook_positions
+        custom_audiences: custom_audiences
       }.reject { |_k, v| v.nil? }
     end
   end
